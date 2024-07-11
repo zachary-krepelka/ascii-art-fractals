b10f87c4be1539506c2be7ecb5682307dec3d982
Zachary Krepelka <155400781+zachary-krepelka@users.noreply.github.com>
2024-07-10 21:53:54 -0400

    wrote a makefile

--- newton/time.sh
+++ newton/time.sh
@@ -3,30 +3,29 @@
 # FILENAME: time.sh
 # AUTHOR: Zachary Krepelka
 # DATE: Tuesday, March 19th, 2024
+# UPDATED: Wednesday, July 10th, 2024 at 9:23 PM
 # ABOUT: a project for the exploration of programming languages
 # ORIGIN: https://github.com/zachary-krepelka/ascii-art-fractals.git
 
-declare -A languages=(
-
-	# key = the name of a programming language
-	# value = the command to execute a file in that language
-
-	[CoffeeScript]='coffee newton.coffee'
-	[JavaScript]='node newton.js'
-	[Java]='java Newton' # first run javac Newton.java
-	[Julia]='julia newton.jl'
-	[Kotlin]='kotlin NewtonKt' # first run kotlinc newton.kt
-	[Lua]='lua newton.lua'
-	[MATLAB]='cmd.exe /c matlab -nojvm -batch newton' # I'm on WSL.
-	[PHP]='php newton.php'
-	[Python]='python newton.py'
-	[Ruby]='ruby newton.rb'
-
+langs=(
+	'Java'
+	'JavaScript'
+	'CoffeeScript'
+	'Lua'
+	'Python'
+	'Ruby'
+	'Julia'
+	'PHP'
+	'Kotlin'
+	'MATLAB'
 )
 
-for i in "${!languages[@]}"
+for lang in ${langs[@]}
 do
-	printf "${i} "
-	echo `{ time ${languages[$i]} ; } 2>&1 1>/dev/null` | cut -c 8-13
+	printf "$lang "
+
+	lang=$(tr '[A-Z]' '[a-z]' <<< $lang)
+
+	echo $({ time make $lang ; } 2>&1 >/dev/null) | cut -c 8-13
 
 done | sort -k 2 | column -t -N LANGUAGE,TIME
